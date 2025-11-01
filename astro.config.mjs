5529611396909168a35ed0d1158fb3adb910b8fd
jimminiglitch <kungfufilla@gmail.com>
2025-11-01 14:53:50 -0400

    fix: simplify astro config for successful build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,5 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
-  vite: {
-    build: {
-      cssMinify: 'lightningcss',
-      minify: 'terser'
-    }
-  }
+  adapter: vercel()
 });
